9470966fb3b8210a5d90326b858b9fbf0760f415
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2023-11-22 22:07:37 +0100

    add server side data fetch

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,10 +1,9 @@
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async () => {
+/** @type {import('./$types').PageServerLoad} */
+export const load = async () => {
 	const response = await fetch('http://localhost:5230/computers');
     const items = await response.json();
 	return {
 		status: 200,
-		data: items,
+		pcs: items,
 	};
 };
