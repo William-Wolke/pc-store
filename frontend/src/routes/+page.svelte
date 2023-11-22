48a0844bf9796ba92d7f54f720bb70a55513deae
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2023-11-22 22:08:43 +0100

    display fetched data on page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,13 @@
-<script lang="ts">
+<script>
 	export const status = 200;
-	export const data: any = [];
+	export let data;
+	const pcs = data.pcs || [];
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	{#each pcs as item}
+		<div>{item.name}</div>
+	{/each}
+</div>
